c0b62038e01d5f391c7f79019bc8bb5bd4dca442
eric <ericmateo.marinzabala@gmail.com>
2024-07-17 09:11:40 -0500

    feat: added walkman buttons and their styles ✨

--- src/components/walkman.jsx
+++ src/components/walkman.jsx
@@ -1,5 +1,11 @@
 import '../styles/walkman.css'
 
+
+import buttonPlay from '../assets/sprites/button-play.png'
+import buttonRewind from '../assets/sprites/button-rewind.png'
+import buttonPause from '../assets/sprites/button-pause.png'
+import buttonControls from '../assets/sprites/buttons-controls.png'
+
 export default function Walkman() {
  
 
@@ -10,6 +16,14 @@ export default function Walkman() {
         <div className='capstan'></div>
         
         <div className='cassette-door'></div>
+
+        <img className='button-play' src={buttonPlay} alt="boton" draggable='false' />
+
+        <img className='button-rewind' src={buttonRewind} alt="boton" draggable='false' />
+
+        <img className='button-pause' src={buttonPause} alt="boton" draggable='false' />
+
+        <img className='buttons-controls' src={buttonControls} alt="boton" draggable='false' />
       
       </div>
     </div>
