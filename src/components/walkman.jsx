e27dc009ecf31fbd289385afe0aee2343209414b
eric <ericmateo.marinzabala@gmail.com>
2024-07-23 07:33:19 -0500

    feat: added drag and drop functionality to the walkman ✨

--- src/components/walkman.jsx
+++ src/components/walkman.jsx
@@ -1,15 +1,18 @@
 import '../styles/walkman.css'
 import { useState, useRef, useEffect } from 'react'
+import { useDroppable } from '@dnd-kit/core'
+import { useDraggable } from '@dnd-kit/core'
 
 
+export default function Walkman({ openDoor }) {
 
-export default function Walkman() {
-  
   const [buttonPicked, setbuttonPicked] = useState('')
 
   const buttonPlay = useRef()
   const buttonRewind = useRef()
   const buttonPause = useRef()
+
+  const cassetteDoor = useRef()
 
   const buttons = [buttonPlay, buttonRewind, buttonPause]
   
@@ -42,14 +45,38 @@ export default function Walkman() {
   
   
   
+  const {isOver, setNodeRef: setDroppableNodeRef} = useDroppable({
+    id: 'droppable-cassette-door',
+  });
   
+ 
+
+  const {attributes, listeners, setNodeRef: setDraggableNodeRef, transform} = useDraggable({
+    id: 'draggable-cassette-door',
+  });
+  const draggableStyle = transform ? {
+    transform: `translate3d(0px, ${transform.y}px, 0)`,
+  } : undefined;
+
   return (
     <div className='container-walkman'>
       <div className='walkman-body sprite-rendering'>
 
         <div className='capstan'></div>
+
+        <div className='door-colision' ref={setDroppableNodeRef} ></div>
         
-        <div className='cassette-door'></div>
+        <div className='container-door'>
+          
+          <div className={`cassette-door ${openDoor ? 'cassette-door-open' : ''}`} 
+            ref={setDraggableNodeRef} 
+            style={draggableStyle}
+            {...attributes}
+            {...listeners}
+          
+          ></div>
+        </div>
+        
 
         <div 
           className={`buttons button-play ${buttonPicked === 'button-play' ? 'button-press' : ''}`} 
@@ -58,6 +85,8 @@ export default function Walkman() {
           }}  
           ref={buttonPlay}
         ></div>
+
+        
 
         <div 
           className={`buttons button-rewind ${buttonPicked === 'button-rewind' ? 'button-press' : ''}`}  
@@ -78,7 +107,6 @@ export default function Walkman() {
         <div className='buttons buttons-controls'></div>
 
         <span className='walkman-meal-decoration sprite-rendering'></span>
-
       </div>
     </div>
   )
